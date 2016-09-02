7c04b4fe8efbd9793d49250ff01e45d79c8691b4
Mafii <mathy.2011.mf@gmail.com>
2016-09-02 17:05:59 +0200

    Add a button dynamically from code-behind to root
    I figured out how dynamically adding and accessing ui controls works.
    In the code behind there is an example now of how to do so.
    This techinque will be used to create most of the ui, so it is very important!

--- NEAT_Visualizer/Views/MainWindow.xaml.cs
+++ NEAT_Visualizer/Views/MainWindow.xaml.cs
@@ -1,16 +1,34 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using NEAT_Visualizer.Interaction.Services;
 
 namespace NEAT_Visualizer.Views
 {
   public class MainWindow : Window
   {
+    private Panel rootPanel;
+
     public MainWindow()
     {
       this.InitializeComponent();
       App.AttachDevTools(this);
 
-      this.LogicalChildren.Add(new Button());
+      var button  = new Button();
+      button.Content = "Wew lad";
+      button.Width = 60;
+      button.Height = 60;
+      rootPanel = this.Find<Panel>("Root");
+      rootPanel.Children.Add(button);
+      button.Click += ButtonOnClick; 
+      //obv, this should be handled in the viewmodel, not in an eventhandler.
+      //this is just for testing/trying out.
+    }
+
+    private void ButtonOnClick(object sender, RoutedEventArgs e)
+    { 
+      (sender as Button).Content = "Wehew!";
     }
 
     private void InitializeComponent()

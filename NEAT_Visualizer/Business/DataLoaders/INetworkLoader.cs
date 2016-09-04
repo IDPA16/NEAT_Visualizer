519cd120f10bc22b077919cfac2687b77d2856dd
Mafii <mathy.2011.mf@gmail.com>
2016-09-04 19:45:24 +0200

    Remove folder DataHandler
    This folder and it's files werent added to the .sln anyways.
    They should've been removed way earlier on. All information is now in the DataLoaders folder.

--- NEAT_Visualizer/Business/DataLoaders/INetworkLoader.cs
+++ NEAT_Visualizer/Business/DataLoaders/INetworkLoader.cs
@@ -1,9 +1,15 @@
-﻿using NEAT_Visualizer.Model;
+﻿using System.Collections.Generic;
+using System.IO;
+using NEAT_Visualizer.Model;
 
 namespace NEAT_Visualizer.Business.DataLoaders
 {
   public interface INetworkLoader
   {
-    NeuralNetwork GetNetwork(string json);
+    NeuralNetwork GetNetwork(FileInfo fileName);
+
+    Species LoadSpecies(DirectoryInfo directory);
+
+    List<Species> LoadFullNeatData(DirectoryInfo rootDirectory);
   }
 }

08eb1d460ee562fb325fa8580dda2ec9533d4cd3
Mafii <mathy.2011.mf@gmail.com>
2016-09-04 19:52:20 +0200

    Add json example and jsonloader stub
    Example comes from JNF_NEAT_Examples after a few runs.

---
+++ NEAT_Visualizer/Business/DataLoaders/NetworkLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NEAT_Visualizer.Model;
+
+namespace NEAT_Visualizer.Business.DataLoaders
+{
+  public class NetworkLoader : INetworkLoader
+  {
+    private class JsonRepresentation
+    {
+      //here comes the native json representation to make deserializing easier
+    }
+
+    public NeuralNetwork GetNetwork(FileInfo fileName)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public Species LoadSpecies(DirectoryInfo directory)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public List<Species> LoadFullNeatData(DirectoryInfo rootDirectory)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}

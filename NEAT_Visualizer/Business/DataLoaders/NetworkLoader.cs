475e3a7d12b950771714e730a1eb299c6d3f1550
Mafii <mathy.2011.mf@gmail.com>
2016-09-04 22:33:32 +0200

    Add current version of json examples with different sizes
    The old file was not correct/contained a bug that has been fixed long time ago.
    Cheerios to @SirRade for handing me the json over (im too lazy to create it on my own)!

--- NEAT_Visualizer/Business/DataLoaders/NetworkLoader.cs
+++ NEAT_Visualizer/Business/DataLoaders/NetworkLoader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using NEAT_Visualizer.Model;
 
@@ -8,22 +9,96 @@ namespace NEAT_Visualizer.Business.DataLoaders
   {
     private class JsonRepresentation
     {
-      //here comes the native json representation to make deserializing easier
+      public class Rootobject
+      {
+        public int populationSize { get; set; }
+        public int generationsPassed { get; set; }
+        public Species[] species { get; set; }
+      }
+
+      public class Species
+      {
+        public float fitnessHighscore { get; set; }
+        public int numberOfStagnantGenerations { get; set; }
+        public Representative representative { get; set; }
+        public Population[] population { get; set; }
+      }
+
+      public class Representative
+      {
+        public float fitness { get; set; }
+        public float fitnessModifier { get; set; }
+        public Network network { get; set; }
+      }
+
+      public class Network
+      {
+        public Neuron[] neurons { get; set; }
+        public Genome[] genome { get; set; }
+      }
+
+      public class Neuron
+      {
+        public int layer { get; set; }
+        public float lastActionPotential { get; set; }
+      }
+
+      public class Genome
+      {
+        public int historicalMarking { get; set; }
+        public int from { get; set; }
+        public int to { get; set; }
+        public float weight { get; set; }
+        public bool isEnabled { get; set; }
+        public bool isRecursive { get; set; }
+      }
+
+      public class Population
+      {
+        public float fitness { get; set; }
+        public float fitnessModifier { get; set; }
+        public Network1 network { get; set; }
+      }
+
+      public class Network1
+      {
+        public Neuron1[] neurons { get; set; }
+        public Genome1[] genome { get; set; }
+      }
+
+      public class Neuron1
+      {
+        public int layer { get; set; }
+        public float lastActionPotential { get; set; }
+      }
+
+      public class Genome1
+      {
+        public int historicalMarking { get; set; }
+        public int from { get; set; }
+        public int to { get; set; }
+        public float weight { get; set; }
+        public bool isEnabled { get; set; }
+        public bool isRecursive { get; set; }
+      }
     }
 
     public NeuralNetwork GetNetwork(FileInfo fileName)
     {
-      throw new System.NotImplementedException();
+      string json = File.ReadAllText(fileName.FullName);
+      //JsonRepresentation.Rootobject;
+
+      return null;
     }
 
     public Species LoadSpecies(DirectoryInfo directory)
     {
-      throw new System.NotImplementedException();
+      throw new NotImplementedException();
     }
 
     public List<Species> LoadFullNeatData(DirectoryInfo rootDirectory)
     {
-      throw new System.NotImplementedException();
+      throw new NotImplementedException();
     }
   }
 }

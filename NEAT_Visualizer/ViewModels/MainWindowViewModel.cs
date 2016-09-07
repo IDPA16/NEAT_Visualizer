f20f51ee4bbe93dcee6a263c28ad063e97c8f8e3
Mafii <mathy.2011.mf@gmail.com>
2016-09-07 23:59:14 +0200

    Add DelegateCommand
    Also adds Commands to the viewModel, initialize methods and command
    handlers.

--- NEAT_Visualizer/ViewModels/MainWindowViewModel.cs
+++ NEAT_Visualizer/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using NEAT_Visualizer.Interaction.Commands;
 using NEAT_Visualizer.Interaction.UserInteractions;
 
 namespace NEAT_Visualizer.ViewModels
@@ -7,15 +9,34 @@ namespace NEAT_Visualizer.ViewModels
   public class MainWindowViewModel : ViewModelBase
   {
     //private const char DELIMITER = '\t';
-
+    #region backing fields
     private int selectedGeneration = 0;
 
     private int selectedSpecies = 0;
 
     private int selectedNetwork = 0;
+    #endregion
+    #region ctors and initializers
+    public MainWindowViewModel()
+    {
+      InitCommands();
+    }
 
+    private void InitCommands()
+    {
+      OpenFileCommand = new DelegateCommand(OnOpenFile);
+      OpenFolderCommand = new DelegateCommand(OnOpenFolder);
+    }
+    #endregion
+    #region InteractionRequests
     public static InteractionRequest ShowInfoInteractionRequest { get; } = InteractionRequest.Register();
+    #endregion
+    #region Commands
+    public ICommand OpenFileCommand { get; private set; } 
 
+    public ICommand OpenFolderCommand { get; private set; }
+    #endregion
+    #region Properties
     public int SelectedGeneration
     {
       get { return selectedGeneration; }
@@ -60,5 +81,17 @@ namespace NEAT_Visualizer.ViewModels
     public ObservableCollection<string> Species { get; set; } = new ObservableCollection<string>() { " 1\t110", " 2\t80" };
 
     public ObservableCollection<string> Networks { get; set; } = new ObservableCollection<string>() { " 1\t80", " 2\t80" };
+    #endregion
+    #region CommandHandlers
+    private void OnOpenFile()
+    {
+      
+    }
+
+    private void OnOpenFolder()
+    {
+      
+    }
+    #endregion
   }
 }

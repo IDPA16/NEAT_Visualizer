71854f707d342dc8ead9fe65a19ccd78622ba2b4
Mafii <mathy.2011.mf@gmail.com>
2016-09-11 18:35:30 +0200

    Update Species to have calculated fitness
    It didnt make sense setting the value (also it dint get set
    after deserializing the json to the model classes).
    Now its getting "calculated" (its set as the higest
    existing fitness of any network from the species.

--- NEAT_Visualizer/Model/Species.cs
+++ NEAT_Visualizer/Model/Species.cs
@@ -1,21 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace NEAT_Visualizer.Model
 {
   public class Species
   {
-    public Species(IList<NeuralNetwork> networks, float fitnessHighscore, int numberOfStagnantGenerations)
+    public Species(IList<NeuralNetwork> networks, int numberOfStagnantGenerations)
     {
-      FitnessHighscore = fitnessHighscore;
       NumberOfStagnantGenerations = numberOfStagnantGenerations;
       Networks = networks;
     }
 
-    public Species() : this(new List<NeuralNetwork>(), 0, 0) { }
+    public Species() : this(new List<NeuralNetwork>(), 0) { }
 
     public IList<NeuralNetwork> Networks { get; }
 
-    public float FitnessHighscore { get; set; }
+    public float FitnessHighscore => Networks.Select(n => n.Fitness).Max();
 
     public int NumberOfStagnantGenerations { get; set; }
   }

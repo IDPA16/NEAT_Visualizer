d7f09b003cf41ae1474847e0fe58f43cf47f5e29
Mafii <mathy.2011.mf@gmail.com>
2016-09-11 20:42:48 +0200

    Add NeuralNetworkVisualizer to use instead of canvas
    This adds an extra abstraction level, and the display logic
    for neural networks can be put into that usercontrol.

---
+++ NEAT_Visualizer/UserControls/NeuralNetworkVisualizer.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using NEAT_Visualizer.Model;
+
+namespace NEAT_Visualizer.UserControls
+{
+  public class NeuralNetworkVisualizer : Canvas, INeuralNetworkVisualizer
+  {
+    private NeuralNetwork currentNetwork;
+
+    public NeuralNetworkVisualizer()
+    {
+      this.InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+      AvaloniaXamlLoader.Load(this);
+    }
+
+    public void DisplayNetwork(NeuralNetwork network)
+    {
+      currentNetwork = network;
+    }
+  }
+}

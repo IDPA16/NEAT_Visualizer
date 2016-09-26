0be14d20655ca76a91bdf58a3adc792a731baee5
Mafii <mathy.2011.mf@gmail.com>
2016-09-27 01:01:57 +0200

    Add xml documentation to LazyGenerationProvider

--- NEAT_Visualizer/Business/GenerationProvider/LazyGenerationProvider.cs
+++ NEAT_Visualizer/Business/GenerationProvider/LazyGenerationProvider.cs
@@ -19,6 +19,11 @@ namespace NEAT_Visualizer.Business.GenerationProvider
     private readonly List<GenerationMetadata> metadata;
     private readonly IGenerationLoader loader;
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="generationFiles">the generation files that can be loaded, and the metadata of them</param>
+    /// <param name="loader">the loader required to load the gneration file.</param>
     public LazyGenerationProvider(Dictionary<GenerationMetadata, FileInfo> generationFiles, IGenerationLoader loader)
     {
       this.generationFiles = generationFiles.Values.ToList();
@@ -26,11 +31,19 @@ namespace NEAT_Visualizer.Business.GenerationProvider
       this.loader = loader;
     }
 
+    /// <summary>
+    /// Get a specific generation. This will load the file and read its content
+    /// with the <see cref="loader"/>
+    /// </summary>
+    /// <param name="index">index of the generation that will be returned</param>
+    /// <returns></returns>
     public Generation GetGeneration(int index)
     {
       return loader.LoadGeneration(generationFiles[index]);
     }
 
+    /// <summary> Returns the metadata </summary>
+    /// <returns>Gets the information about the available generations</returns>
     public IList<GenerationMetadata> GetGenerations()
     {
       return metadata;

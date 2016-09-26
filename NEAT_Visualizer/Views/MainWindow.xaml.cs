e6cecaea715041f520ea7a95d7edab4d0698dea1
Mafii <mathy.2011.mf@gmail.com>
2016-09-27 01:17:45 +0200

    Replace Canvas with Path for SVG
    This allows the further usage of svg libraries to
    draw neural networks.

--- NEAT_Visualizer/Views/MainWindow.xaml.cs
+++ NEAT_Visualizer/Views/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using NEAT_Visualizer.Interaction.Services;
@@ -21,16 +22,8 @@ namespace NEAT_Visualizer.Views
       InitializeComponent();
       App.AttachDevTools(this);
 
-      var canvas = this.FindControl<Canvas>("Canvas");
-      canvas.PointerPressed += Canvas_PointerPressed;
-    }
-
-    private void Canvas_PointerPressed(object sender, Avalonia.Input.PointerPressedEventArgs e)
-    {
-      var canvas = sender as Canvas;
-      Point point = e.GetPosition(canvas);
-      InteractionRequest.Register().Raise(new UserInteraction() {Content = point.ToString() , Title = "Point clicked", UserInteractionOptions = UserInteractionOptions.Ok});
-      //(canvas as INeuralNetworkVisualizer).DisplayNetwork((DataContext as MainWindowViewModel).CurrentNetwork);
+      var path = this.FindControl<Path>("SVGHost");     
+      //path.Data = ?
     }
 
     private void InitializeComponent()

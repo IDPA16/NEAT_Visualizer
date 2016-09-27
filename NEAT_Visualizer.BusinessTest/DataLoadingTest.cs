80b923f5fb48249472e8edb5eb6d14fded8e452f
Mafii <mathy.2011.mf@gmail.com>
2016-09-27 13:40:56 +0200

    Create stub for testing DataLoading
    All that is needed now is to actually write the test.

---
+++ NEAT_Visualizer.BusinessTest/DataLoadingTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using NEAT_Visualizer.Business.DataLoaders;
+using Xunit;
+
+namespace NEAT_Visualizer.BusinessTest
+{
+  public class DataLoadingTest : IClassFixture<GenerationLoader>
+  {
+    private GenerationLoader loader;
+
+    public DataLoadingTest(GenerationLoader aLoader)
+    {
+      loader = aLoader;
+    }
+
+    [Fact]
+    public void CanLoadJsonFileWithoutError()
+    {
+    }
+
+    [Fact]
+    public void LoadsAllJsonDataIntoModel()
+    {   
+    }
+  }
+}

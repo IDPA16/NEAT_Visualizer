50f5301c93f6272b2792e69bdf9d3ba093f19ca4
Mafii <mathy.2011.mf@gmail.com>
2016-09-27 12:34:42 +0200

    Add xunit test project

---
+++ NEAT_Visualizer.BusinessTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace NEAT_Visualizer.BusinessTest
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void TestMethod1()
+    {
+    }
+  }
+}

1811a8dae8bc8747b3a9d9e34010feb2c500cbde
Mafii <mafi@fog.im>
2016-10-07 16:56:08 +0200

    Add NetworkPresenter stub

---
+++ NEAT_Visualizer/UserControls/INetworkPresenter.cs
@@ -0,0 +1,9 @@
+﻿using NEAT_Visualizer.Model;
+
+namespace NEAT_Visualizer.UserControls
+{
+  public interface INetworkPresenter
+  {
+    void DisplayNetwork(NeuralNetwork network);
+  }
+}
---
+++ NEAT_Visualizer/UserControls/NetworkPresenter.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using NEAT_Visualizer.Model;
+
+namespace NEAT_Visualizer.UserControls
+{
+  public class NetworkPresenter : UserControl, INetworkPresenter
+  {
+    public NetworkPresenter()
+    {
+      this.InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+      AvaloniaXamlLoader.Load(this);
+    }
+
+    public void DisplayNetwork(NeuralNetwork network)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

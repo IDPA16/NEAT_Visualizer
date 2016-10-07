0eb0eeaf895cd7bb92fb4bc394f84b9216f0e823
Mafii <mafi@fog.im>
2016-10-07 22:45:55 +0200

    Add NetworkPresenter
    And example codes for drawing

--- NEAT_Visualizer/UserControls/NetworkPresenter.xaml.cs
+++ NEAT_Visualizer/UserControls/NetworkPresenter.xaml.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using NEAT_Visualizer.Model;
+using PropertyChanged;
 
 namespace NEAT_Visualizer.UserControls
 {
-  public class NetworkPresenter : UserControl, INetworkPresenter
+  [DoNotNotify]
+  public class NetworkPresenter : Canvas, INetworkPresenter
   {
     public NetworkPresenter()
     {
       this.InitializeComponent();
+      //DisplayNetwork(null);
     }
 
     private void InitializeComponent()
@@ -19,7 +24,19 @@ namespace NEAT_Visualizer.UserControls
 
     public void DisplayNetwork(NeuralNetwork network)
     {
-      throw new NotImplementedException();
+      var canvas = this;/*.FindControl<Canvas>("Canvas");*/
+      var sg = new StreamGeometry();
+      
+
+      using (var sgc = sg.Open())
+      {
+        sgc.BeginFigure(new Point(2, 2), true);
+        sgc.LineTo(new Point(20, 20));
+        sgc.ArcTo(new Point(50,50), new Size(20,20), 0, false, SweepDirection.Clockwise);
+        sgc.EndFigure(false);
+      }
+
+      canvas.Clip = sg;
     }
   }
 }

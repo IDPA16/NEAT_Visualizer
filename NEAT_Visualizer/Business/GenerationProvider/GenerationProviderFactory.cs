f7ebde4fdb1f205200823037b866ec30a1872364
Mafii <mafi@fog.im>
2016-10-22 15:17:07 +0200

    Add Stub for GenerationProviderFactory

---
+++ NEAT_Visualizer/Business/GenerationProvider/GenerationProviderFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NEAT_Visualizer.Business.GenerationProvider
+{
+  public class GenerationProviderFactory
+  {
+    public static IGenerationProvider Get(FileInfo file)
+    {
+      throw new NotImplementedException();
+    }
+
+    public static IGenerationProvider Get(DirectoryInfo directory)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
